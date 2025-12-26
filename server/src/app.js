01df57642b06e7fcffefbbb6151ba46333d3b4a3
Aditya Saputra <aditsputra.cv@gmail.com>
2025-12-26 17:25:38 +0700

    feat: set up Express server with routing for auth, game, and leaderboard

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const authRoutes = require('./routes/auth.routes');
+const gameRoutes = require('./routes/game.routes');
+const leaderboardRoutes = require('./routes/leaderboard.routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/game", gameRoutes);
+app.use("/leaderboard", leaderboardRoutes);
+
+module.exports = app;
